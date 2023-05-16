161c004934aeefce429ce4d18961e4eeefb1c705
Emanuel <116455502+acevedo7@users.noreply.github.com>
2023-05-15 21:46:11 -0300

    Add files via upload

--- frontend/index.php
+++ frontend/index.php
@@ -1,3 +1,4 @@
+
 <?php
 require_once "../config/conexion.php";
 
@@ -38,6 +39,28 @@ if (isset($_POST['usuario']) && isset($_POST['clave'])) {
 <!DOCTYPE html>
 <html>
 <head>
+    <!-- ------------------------------------------------------- -->
+<!-- Agrega los scripts necesarios -->
+<script src="https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.22.0/firebase-analytics.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js"></script>
+
+<!-- Agrega tu configuración de Firebase aquí -->
+<script>
+const firebaseConfig = {
+    apiKey: "AIzaSyAhFfUx9t5N2_mJ4XSjrbv_27A4PgrLNIU",
+    authDomain: "portafolio-5e28f.firebaseapp.com",
+    projectId: "portafolio-5e28f",
+    storageBucket: "portafolio-5e28f.appspot.com",
+    messagingSenderId: "596683497898",
+    appId: "1:596683497898:web:7aa8e5d23d097e9f1b4bd3",
+    measurementId: "G-PNYLG7BE67"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+</script>    
+    <!-- ------------------------------------------------------- -->
 <link rel="stylesheet" href="admin.css">
 <link href="../dist/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://use.fontawesome.com/fefc2fb08e.js"></script>
@@ -59,9 +82,9 @@ if (isset($_POST['usuario']) && isset($_POST['clave'])) {
         <label for="exampleInputEmail1" class="form-label">Clave</label><br>
         <input type="password"  id="inputPassword5" class="form-control" aria-labelledby="passwordHelpBlock" name="clave" placeholder="password" required><br>
     </div>    
-          <input type="submit" class="btn btn-primary" value="Iniciar sesión">
-    
+          <input type="submit" class="btn btn-primary" value="Iniciar sesión"> 
     </form>
+    <a href="logingoogle.php">iniciar con Google</a>
 </div>
 </body>
 </html>
